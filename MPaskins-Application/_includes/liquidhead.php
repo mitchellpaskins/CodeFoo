4a1db5fa126a4744cbe98fe12166caacb68543e1
Mitchell Paskins <mpaskins@hotmail.co.uk>
2012-04-29 18:19:57 +0100

    parent folder

---
+++ MPaskins-Application/_includes/liquidhead.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" /><?php // 				DO NOT FORGET TO TAKE OUT '/' in html!!! ?>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>
+		Mitchell Paskins Code Foo Application
+	</title>
+	<link href="_css/liqmain.css" rel="stylesheet" type="text/css" />
+	<link href="_css/connect.css" rel="stylesheet" type="text/css" />
+    <script src="_scripts/jquery.js" type="text/javascript"></script>
+	<script src="_scripts/script1.js" type="text/javascript"></script>
+	<script src="_scripts/plates.js" type="text/javascript"></script>
+	<script src="_scripts/connect.js" type="text/javascript"></script>
+	<script src="_scripts/heights.js" type="text/javascript"></script>
+</head>
+<?php $public=true; ?>
+<body>
+<div id="frame">
+<div id="header">
+	<div id="title">
+    <p>Code Foo</p>
+    </div>
+    <div id="contact">
+    <p>Mitchell Paskins</p>
+    <p>mpaskins@hotmail.co.uk</p>
+    </div>
+</div>
+<div id="mainSeg">
+	<div id="menu">
+    <a href="cfindex.php"><p class="menuLink">Home</p></a>
+    <a href="cfq1.php"><p class="menuLink">Question 1: Ping Pong</p></a>
+    <a href="cfq2.php"><p class="menuLink">Question 2: Liquid Layout</p></a>
+    <a href="cfq3.php"><p class="menuLink">Question 3: Plates</p></a>
+    <a href="cfq4.php"><p class="menuLink">Question 4: Video</p></a>
+    <a href="cfbonus.php"><p class="menuLinkBot">Bonus: Connect 4</p></a>
+    </div>
+    <div id="content">
+    <div id="whiteLeft">
+    	</div>
+    <div id="innerContent">
---
+++ MPaskins-Application/cfq4.php
@@ -0,0 +1,5 @@
+<?php require_once("_includes/liquidhead.php"); ?>
+
+<object id="video" data="codefoo.m4v" height="550" width="720" />
+
+<?php require_once("_includes/liquidfooter.php"); ?>
